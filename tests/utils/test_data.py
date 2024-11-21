bc79badf1d5ecb81ef9d85f92b3c08aa4d72e8b8
sssohrab <sohrab.ferdowsi@gmail.com>
2024-11-21 15:22:41 +0100

    Implement (iterable) typed dict classes with tests
    Where TypedDict ensures the passed data types comply to what had been
    declared before and TypedDictIterable ensures this along an iterable
    over several items represented in a columnar way.

--- tests/utils/test_data.py
+++ tests/utils/test_data.py
@@ -1,6 +1,11 @@
 from torchbase.utils.data import random_split_iterable, split_to_train_valid_test
+from torchbase.utils.data import TypedDict, TypedDictIterable
+
 import unittest
 from io import StringIO
+
+from enum import Enum
+from dataclasses import dataclass
 
 
 class RandomSplittingUnitTest(unittest.TestCase):
@@ -45,6 +50,192 @@ class RandomSplittingUnitTest(unittest.TestCase):
     def test_split_to_train_valid_test_invalid(self):
         with self.assertRaises(ValueError):
             train, valid = split_to_train_valid_test(self.data_file, (0.7, 0.3))
+
+
+class ExampleCategoricalDataTypeGender(Enum):
+    MALE = 0
+    FEMALE = 1
+    NON_BINARY = -1
+
+    @staticmethod
+    def from_string(value: str | float):
+        if str(value).lower() in [0, "man", "male", "guy", "homme", "gars"]:
+            return ExampleCategoricalDataTypeGender.MALE
+        elif str(value).lower() in [1, "woman", "female", "femme", "meuf"]:
+            return ExampleCategoricalDataTypeGender.FEMALE
+        elif str(value).lower() in [-1, "non-binary", "nonbinary"]:
+            return ExampleCategoricalDataTypeGender.NON_BINARY
+        else:
+            raise ValueError("Could not infer gender from text.")
+
+    def __str__(self):
+        if self.value == 0:
+            return "MALE"
+        elif self.value == 1:
+            return "FEMALE"
+        elif self.value == -1:
+            return "NON_BINARY"
+        else:
+            raise ValueError
+
+
+@dataclass
+class ExampleNestedDataClass:
+    email: str
+    phone_number: str
+    job_title: str | None
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            email=data.get("email"),
+            phone_number=data.get("phone_number"),
+            job_title=data.get("job_title"),
+        )
+
+
+class TypedDictUnitTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.typed_dict = TypedDict({
+            "name": str,
+            "age": int,
+            "gender": ExampleCategoricalDataTypeGender,
+            "details": ExampleNestedDataClass | None
+        })
+
+    def test_correct_types(self):
+        input_dict = {
+            "name": "John Foo",
+            "age": 43,
+            "gender": ExampleCategoricalDataTypeGender.MALE,
+            "details": ExampleNestedDataClass.from_dict({"email": "john@foo.him",
+                                                         "phone_number": "+41-22-22",
+                                                         "job_title": "Lead Expert of Staff"})
+        }
+
+        output_dict = self.typed_dict(input_dict)
+        self.assertEqual(input_dict, output_dict)
+
+    def test_incorrect_type(self):
+        input_dict = {
+            "name": "Jane Foo",
+            "age": "43",
+            "gender": ExampleCategoricalDataTypeGender.FEMALE,
+            "details": None
+        }
+        with self.assertRaises(TypeError):
+            self.typed_dict(input_dict)
+
+    def test_allowed_none(self):
+        input_dict = {
+            "name": "John Foo",
+            "age": 43,
+            "gender": ExampleCategoricalDataTypeGender.from_string("man"),
+            "details": None
+        }
+
+        output_dict = self.typed_dict(input_dict)
+        self.assertEqual(input_dict, output_dict)
+
+    def test_not_allowed_none(self):
+        input_dict = {
+            "name": "John Foo",
+            "age": 43,
+            "gender": None,
+            "details": ExampleNestedDataClass.from_dict({"email": "john@foo.him",
+                                                         "phone_number": "+41-22-22",
+                                                         "job_title": "Lead Expert of Staff"})
+        }
+        with self.assertRaises(TypeError):
+            self.typed_dict(input_dict)
+
+
+class TypedDictIterableUnitTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.typed_dict_iterable = TypedDictIterable({
+            "name": str,
+            "age": int,
+            "gender": ExampleCategoricalDataTypeGender,
+            "details": ExampleNestedDataClass | None
+
+        })
+
+    def test_correct_types(self):
+        input_dict = {
+            "name": ["John Foo", "Jim Bar"],
+            "age": [43, 22],
+            "gender": [ExampleCategoricalDataTypeGender.MALE, ExampleCategoricalDataTypeGender.NON_BINARY],
+            "details": [ExampleNestedDataClass.from_dict({"email": "john@foo.him",
+                                                          "phone_number": "+41-22-22",
+                                                          "job_title": "Lead Expert of Staff"}), None]
+        }
+
+        output_dict = self.typed_dict_iterable(input_dict)
+        self.assertEqual(input_dict, output_dict)
+
+    def test_incorrect_type(self):
+        input_dict = {
+            "name": ["John Foo", "Jim Bar"],
+            "age": [43, "22"],
+            "gender": [ExampleCategoricalDataTypeGender.MALE, ExampleCategoricalDataTypeGender.NON_BINARY],
+            "details": ["None", None]
+        }
+        with self.assertRaises(TypeError):
+            self.typed_dict_iterable(input_dict)
+
+    def test_incorrect_iterable(self):
+        input_dict = {
+            "name": ["John Foo", "Jim Bar"],
+            "age": "AZ",
+            "gender": [ExampleCategoricalDataTypeGender.MALE, ExampleCategoricalDataTypeGender.NON_BINARY],
+            "details": [None, None]
+        }
+        with self.assertRaises(TypeError):
+            self.typed_dict_iterable(input_dict)
+
+    def test_no_iterable(self):
+        input_dict = {
+            "name": ["John Foo", "Jim Bar"],
+            "age": 23,
+            "gender": [ExampleCategoricalDataTypeGender.MALE, ExampleCategoricalDataTypeGender.NON_BINARY],
+            "details": [None, None]
+        }
+        with self.assertRaises(TypeError):
+            self.typed_dict_iterable(input_dict)
+
+    def test_variable_length_iterables(self):
+        input_dict = {
+            "name": ["John Foo", "Jim Bar"],
+            "age": [43, 22, 13],
+            "gender": [ExampleCategoricalDataTypeGender.MALE, ExampleCategoricalDataTypeGender.NON_BINARY],
+            "details": [None]
+        }
+        with self.assertRaises(ValueError):
+            self.typed_dict_iterable(input_dict)
+
+    def test_data_attribute_valid_set_and_get(self):
+        input_dict = {
+            "name": ["John Foo", "Jim Bar"],
+            "age": [43, 22],
+            "gender": [ExampleCategoricalDataTypeGender.MALE, ExampleCategoricalDataTypeGender.NON_BINARY],
+            "details": [None, None]
+        }
+
+        self.typed_dict_iterable.data = input_dict
+        self.assertEqual(input_dict, self.typed_dict_iterable.data)
+
+    def test_data_attribute_invalid_set_and_get(self):
+        input_dict = {
+            "name": ["John Foo", "Jim Bar"],
+            "age": [43],
+            "gender": [ExampleCategoricalDataTypeGender.MALE, ExampleCategoricalDataTypeGender.NON_BINARY],
+            "details": [None, None]
+        }
+
+        with self.assertRaises(ValueError):
+            self.typed_dict_iterable.data = input_dict
 
 
 if __name__ == "__main__":
--- torchbase/utils/data.py
+++ torchbase/utils/data.py
@@ -1,5 +1,6 @@
 import random
-from typing import Tuple, Iterable, List, Any
+from typing import Tuple, Iterable, List, Any, Dict, Type, get_args
+import types
 
 
 def random_split_iterable(iterable_input: Iterable[Any], portions: Tuple[float, ...]) -> Tuple[List[Any], ...]:
@@ -46,3 +47,95 @@ def split_to_train_valid_test(iterable_input: Iterable, portions: Tuple[float, f
     train, valid, test = random_split_iterable(iterable_input, portions=portions)
 
     return train, valid, test
+
+
+class TypedDict:
+    def __init__(self, type_dict: Dict[str, Type]):
+        if not isinstance(type_dict, Dict):
+            raise TypeError("type_dict should be a dictionary.")
+        for key, value in type_dict.items():
+            if not isinstance(key, str):
+                raise TypeError("key `{}` of type_dict should be a string.".format(key))
+            if not isinstance(value, type) and not isinstance(value, types.UnionType):
+                raise TypeError("Specify the expected type for the key `{}`. Union of types allowed".format(key))
+
+        self.type_dict = type_dict
+
+    def check_type(self, key: str, value: Any) -> None:
+        if key not in self.type_dict.keys():
+            raise KeyError("`{}` not a recognized key.".format(key))
+
+        expected_type = self.type_dict[key]
+        none_allowed = type(None) in get_args(expected_type)
+
+        if value is None and not none_allowed:
+            raise TypeError("Value for `{}` is not allowed to be None.".format(key))
+
+        if not isinstance(value, expected_type):
+            raise TypeError("Got the unexpected type {} for the key {}.".format(type(value).__name__, key))
+
+    def __call__(self, data_dict: Dict[str, Any]) -> Dict[str, Any]:
+        for key, value in data_dict.items():
+            self.check_type(key, value)
+
+        for key in self.type_dict.keys():
+            if key not in data_dict.keys():
+                raise TypeError(
+                    "The declared key `{}` in type_dict is missing in the passed data_dict.".format(key))
+
+        return data_dict
+
+
+class TypedDictIterable(TypedDict):
+
+    def __init__(self, type_dict: Dict[str, Type]):
+        super().__init__(type_dict)
+        self._data = None
+
+    def check_type(self, key: str, value: Any) -> None:
+        if key not in self.type_dict.keys():
+            raise KeyError("`{}` not a recognized key.".format(key))
+
+        expected_type = self.type_dict[key]
+        none_allowed = type(None) in get_args(expected_type)
+
+        if value is None and not none_allowed:
+            raise TypeError("Value for `{}` is not allowed to be None.".format(key))
+
+        if isinstance(value, (str, bytes)):
+            raise TypeError(
+                "Value for `{}` is expected to be an iterable of {}, got a string/bytes instead.".format(
+                    key, expected_type.__name__))
+        if not isinstance(value, Iterable):
+            raise TypeError(
+                "Value for `{}` is expected to be an iterable of {}, got non-iterable.".format(key,
+                                                                                               expected_type.__name__))
+        for item in value:
+            if not isinstance(item, expected_type):
+                raise TypeError(
+                    "Expected items in `{}` to be `{}`, got `{}` instead.".format(key, expected_type.__name__,
+                                                                                  type(item).__name__))
+
+    def __call__(self, data_dict: Dict[str, Iterable[Any]]) -> Dict[str, Iterable[Any]]:
+        for key in self.type_dict.keys():
+            if key not in data_dict:
+                raise KeyError("The declared key `{}` in type_dict is missing in the passed data_dict.".format(key))
+
+        last_iterable_length = -1
+        for idx, (key, value) in enumerate(data_dict.items()):
+            self.check_type(key, value)
+            this_iterable_length = len(list(value))
+            if idx != 0 and this_iterable_length != last_iterable_length:
+                raise ValueError("The length of all iterables over values must be the same.")
+
+            last_iterable_length = this_iterable_length
+
+        return data_dict
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value: Dict[str, Any]):
+        self._data = self.__call__(value)
