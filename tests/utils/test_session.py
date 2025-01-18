075d681c5b66217bd46df40b2f4798f1bf7400bf
sssohrab <sohrab.ferdowsi@gmail.com>
2025-01-18 14:32:36 +0100

    Implement function to validate TB custom logging
    and some simple tests.

--- tests/utils/test_session.py
+++ tests/utils/test_session.py
@@ -1,6 +1,37 @@
 from torchbase.utils.session import TrainingConfigSessionDict
+from torchbase.utils.session import is_custom_scalar_logging_layout_valid
 
 import unittest
+
+
+class CustomScalarLoggingLayoutValidityUnitTest(unittest.TestCase):
+    def test_custom_scalar_logging_layout_valid(self):
+        layout = {
+            'Loss': {
+                'Loss (train vs val)': ['Multiline', ['training/loss/epochs', 'validation-valid/loss/epochs']],
+                'Loss valid (with and without aug)': ['Line', ['validation-valid/loss/epochs',
+                                                               'validation-valid_with_augmentation/loss/epochs']],
+            }
+        }
+
+        self.assertTrue(is_custom_scalar_logging_layout_valid(layout,
+                                                              validation_dataset_names=("valid",
+                                                                                        "valid_with_augmentation"),
+                                                              metric_names=("precision", "recall")))
+
+    def test_custom_scalar_logging_layout_invalid(self):
+        layout = {
+            'Loss': {
+                'Loss (train vs val)': ['Multiline', ['train/loss/epochs', 'validation-valid/loss/epochs']],
+                'Loss valid (with and without aug)': ['Line', ['validation-valid/loss/epochs',
+                                                               'validation-valid_with_augmentation/loss/epochs']],
+            }
+        }
+
+        self.assertFalse(is_custom_scalar_logging_layout_valid(layout,
+                                                               validation_dataset_names=("valid",
+                                                                                         "valid_with_augmentation"),
+                                                               metric_names=("precision", "recall")))
 
 
 class TrainingConfigSessionDictUnitTest(unittest.TestCase):
